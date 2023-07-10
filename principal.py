328b8aae2a3f77650bb0e295a1e227ec611a02df
Gabriel Requelme <garequelmero@uide.edu.ec>
2023-07-09 22:32:19 -0500

    Prueba de subida

--- principal.py
+++ principal.py
@@ -1,1 +1,14 @@
 #Archivo Python para clasificacion de imagenes - Prueba final
+
+#Instalacion de Junyper en el ambiente
+#>pip install jupyter
+
+#Iniciar Juniper
+#>jupyter notebook
+
+#Instalando paquetes
+#>pip install numpy
+#>pip install matplotlib
+#>pip install scikit-learn
+#>pip install keras
+#>pip install tensorflow
